adcd9e1a0d59813412de27aa50b8918bbe60ed52
Mehran Hamza <Mehranhamza8@gmail.com>
2024-11-28 00:29:42 +0500

    update

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from any origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+)
 @app.get('/')
 def hello_world():
     return "hello world"
